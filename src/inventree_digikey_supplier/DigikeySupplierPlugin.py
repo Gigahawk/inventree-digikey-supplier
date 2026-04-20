bc1f65e0cef629a3d0061b618de97565359808a6
Jasper Chan <jasperchan515@gmail.com>
2026-04-20 14:13:53 -0700

    feature: use digikey api

--- src/inventree_digikey_supplier/DigikeySupplierPlugin.py
+++ src/inventree_digikey_supplier/DigikeySupplierPlugin.py
@@ -1,6 +1,9 @@
 """Sample supplier plugin."""
 
 from django.conf import settings
+from digikey_api_v4.api import DigikeyClient
+from digikey_api_v4.models import KeywordRequest
+from bravado.exception import HTTPNotFound
 
 from company.models import Company, ManufacturerPart, SupplierPart, SupplierPriceBreak
 from part.models import Part
@@ -11,9 +14,9 @@ from plugin.plugin import InvenTreePlugin
 class DigikeySupplierPlugin(SupplierMixin, InvenTreePlugin):
     """Example plugin to integrate with a dummy supplier."""
 
-    NAME = "DigikeySampleSupplierPlugin"
-    SLUG = "digikeysamplesupplier"
-    TITLE = "Sample DigiKey supplier plugin"
+    NAME = "DigikeySupplierPlugin"
+    SLUG = "digikeysupplier"
+    TITLE = "DigiKey supplier plugin"
 
     VERSION = "0.0.2"
 
@@ -23,98 +26,113 @@ class DigikeySupplierPlugin(SupplierMixin, InvenTreePlugin):
             "description": "Enable downloading of part images during import (not recommended during testing)",
             "validator": bool,
             "default": False,
-        }
+        },
+        "CLIENT_ID": {
+            "name": "Client ID",
+            "description": "DigiKey API V4 Client ID",
+            "default": "",
+            "reqiured": True,
+        },
+        "CLIENT_SECRET": {
+            "name": "Client Secret",
+            "description": "DigiKey API V4 Client Secret",
+            "default": "",
+            "reqiured": True,
+        },
     }
 
     def __init__(self):
         """Initialize the sample supplier plugin."""
         super().__init__()
 
-        self.sample_data = []
-        for material in ["DkSteel", "DkAluminium", "DkBrass"]:
-            for size in ["M1", "M2", "M3", "M4", "M5"]:
-                for length in range(5, 30, 5):
-                    self.sample_data.append(
-                        {
-                            "material": material,
-                            "thread": size,
-                            "length": length,
-                            "sku": f"BOLT-{material}-{size}-{length}",
-                            "name": f"Bolt {size}x{length}mm {material}",
-                            "description": f"DIGIKEY This is a sample part description demonstration purposes for the {size}x{length} {material} bolt.",
-                            "price": {
-                                1: [1.0, "EUR"],
-                                10: [0.9, "EUR"],
-                                100: [0.8, "EUR"],
-                                5000: [0.5, "EUR"],
-                            },
-                            "link": f"https://example.com/sample-part-{size}-{length}-{material}",
-                            "image_url": r"https://github.com/inventree/demo-dataset/blob/main/media/part_images/flat-head.png?raw=true",
-                            "brand": "Bolt Manufacturer",
-                        }
-                    )
+    @property
+    def _client(self):
+        return DigikeyClient(
+            client_id=self.get_setting("CLIENT_ID"),
+            client_secret=self.get_setting("CLIENT_SECRET"),
+            sandbox=False,
+        )
 
     def get_suppliers(self) -> list[supplier.Supplier]:
         """Return a list of available suppliers."""
-        return [supplier.Supplier(slug="sample-fasteners", name="Sample Fasteners")]
+        return [supplier.Supplier(slug="digikey", name="DigiKey")]
 
     def get_search_results(
         self, supplier_slug: str, term: str
     ) -> list[supplier.SearchResult]:
         """Return a list of search results based on the search term."""
+        results = self._client.keyword_search(body=KeywordRequest(Keywords=term))
+        products = results.Products
         return [
             supplier.SearchResult(
-                sku=p["sku"],
-                name=p["name"],
-                description=p["description"],
-                exact=p["sku"] == term,
-                price=f"{p['price'][1][0]:.2f}€",
-                link=p["link"],
-                image_url=p["image_url"],
+                # TODO: In theory MPN may not be unique across manufacturers.
+                # Unfortunately it doesn't really seem like DigiKey offers a
+                # guaranteed unique identifier per part other than the DigiKey
+                # part number but there are usually a few depending on packaging
+                # options
+                sku=p.ManufacturerProductNumber,
+                name=p.Description.ProductDescription,
+                exact=p.ManufacturerProductNumber == term,
+                description=p.Description.DetailedDescription,
+                # TODO: use locale price to format this?
+                # Don't format to 2 decimal places, real parts may cost less than 1c
+                price=f"${p.UnitPrice}",
+                link=p.ProductUrl,
+                image_url=p.PhotoUrl,
                 existing_part=getattr(
-                    SupplierPart.objects.filter(SKU=p["sku"]).first(), "part", None
+                    SupplierPart.objects.filter(
+                        SKU=p.ManufacturerProductNumber
+                    ).first(),
+                    "part",
+                    None,
                 ),
             )
-            for p in self.sample_data
-            if all(t.lower() in p["name"].lower() for t in term.split())
+            for p in products
         ]
 
     def get_import_data(self, supplier_slug: str, part_id: str):
         """Return import data for a specific part ID."""
-        for p in self.sample_data:
-            if p["sku"] == part_id:
-                p = p.copy()
-                p["variants"] = [
-                    x["sku"]
-                    for x in self.sample_data
-                    if x["thread"] == p["thread"] and x["length"] == p["length"]
-                ]
-                return p
-
+        try:
+            return self._client.product_details(part_id)
+        except HTTPNotFound:
+            raise supplier.PartNotFoundError()
         raise supplier.PartNotFoundError()
 
     def get_pricing_data(self, data) -> dict[int, tuple[float, str]]:
         """Return pricing data for the given part data."""
-        return data["price"]
+        # DigiKey has different breaks depending on variation.
+        # Just use the lowest price listed for identical breaks
+        # TODO: use locale or something to format dollar amount
+
+        out = {}
+        for pv in data.ProductVariations:
+            for pb in pv.StandardPricing:
+                qty = int(pb.BreakQuantity)
+                price = float(pb.UnitPrice)
+                existing = out.get(qty, (999999, "CAD"))
+                if existing[0] > price:
+                    out[qty] = (price, "CAD")
+        return out
 
     def get_parameters(self, data) -> list[supplier.ImportParameter]:
         """Return a list of parameters for the given part data."""
         return [
-            supplier.ImportParameter(name="Thread", value=data["thread"][1:]),
-            supplier.ImportParameter(name="Length", value=f"{data['length']}mm"),
-            supplier.ImportParameter(name="Material", value=data["material"]),
-            supplier.ImportParameter(name="Head", value="Flat Head"),
+            supplier.ImportParameter(
+                name=p.ParameterText,
+                value=p.ValueText,
+            )
+            for p in data.Parameters
         ]
 
     def import_part(self, data, **kwargs) -> Part:
         """Import a part based on the provided data."""
         part, created = Part.objects.get_or_create(
-            name__iexact=data["sku"],
+            name__iexact=data.ManufacturerProductNumber,
             purchaseable=True,
             defaults={
-                "name": data["sku"],
-                "description": data["description"],
-                "link": data["link"],
+                "name": data.Description.ProductDescription,
+                "description": data.Description.DetailedDescription,
+                "link": data.ProductUrl,
                 **kwargs,
             },
         )
@@ -123,59 +141,62 @@ class DigikeySupplierPlugin(SupplierMixin, InvenTreePlugin):
         if created:
             # Prevent downloading images during testing, as this can lead to unreliable tests
             if (
-                data["image_url"]
+                data.PhotoUrl
                 and not settings.TESTING
                 and self.get_setting("DOWNLOAD_IMAGES")
             ):
-                file, fmt = self.download_image(data["image_url"])
+                file, fmt = self.download_image(data.PhotoUrl)
                 filename = f"part_{part.pk}_image.{fmt.lower()}"
                 part.image.save(filename, file)
 
-            # link other variants if they exist in our inventree database
-            if len(data["variants"]):
-                # search for other parts that may already have a template part associated
-                variant_parts = [
-                    x.part
-                    for x in SupplierPart.objects.filter(SKU__in=data["variants"])
-                ]
-                parent_part = self.get_template_part(
-                    variant_parts,
-                    {
-                        # we cannot extract a real name for the root part, but we can try to guess a unique name
-                        "name": data["sku"].replace(data["material"] + "-", ""),
-                        "description": data["name"].replace(" " + data["material"], ""),
-                        "link": data["link"],
-                        "image": part.image.name,
-                        "is_template": True,
-                        **kwargs,
-                    },
-                )
+            # TODO: What does variants mean in this context?
+            # I don't think it means the same thing as ProductVariations
+            ## link other variants if they exist in our inventree database
+            # if len(data["variants"]):
+            #    # search for other parts that may already have a template part associated
+            #    variant_parts = [
+            #        x.part
+            #        for x in SupplierPart.objects.filter(SKU__in=data["variants"])
+            #    ]
+            #    parent_part = self.get_template_part(
+            #        variant_parts,
+            #        {
+            #            # we cannot extract a real name for the root part, but we can try to guess a unique name
+            #            "name": data["sku"].replace(data["material"] + "-", ""),
+            #            "description": data["name"].replace(" " + data["material"], ""),
+            #            "link": data["link"],
+            #            "image": part.image.name,
+            #            "is_template": True,
+            #            **kwargs,
+            #        },
+            #    )
 
-                # after the template part was created, we need to refresh the part from the db because its tree id may have changed
-                # which results in an error if saved directly
-                part.refresh_from_db()
-                part.variant_of = parent_part
-                part.save()
+            #    # after the template part was created, we need to refresh the part from the db because its tree id may have changed
+            #    # which results in an error if saved directly
+            #    part.refresh_from_db()
+            #    part.variant_of = parent_part
+            #    part.save()
 
         return part
 
     def import_manufacturer_part(self, data, **kwargs) -> ManufacturerPart:
         """Import a manufacturer part based on the provided data."""
         mft, _ = Company.objects.get_or_create(
-            name__iexact=data["brand"],
+            name__iexact=data.Manufacturer.Name,
             defaults={
                 "is_manufacturer": True,
                 "is_supplier": False,
-                "name": data["brand"],
+                "name": data.Manufacturer.Name,
             },
         )
 
         mft_part, created = ManufacturerPart.objects.get_or_create(
-            MPN=f"MAN-{data['sku']}", manufacturer=mft, **kwargs
+            MPN=f"MAN-{data.ManufacturerProductNumber}", manufacturer=mft, **kwargs
         )
 
         if created:
             # Attachments, notes, parameters and more can be added here
+            # TODO: Add datasheet link or something?
             pass
 
         return mft_part
@@ -183,10 +204,10 @@ class DigikeySupplierPlugin(SupplierMixin, InvenTreePlugin):
     def import_supplier_part(self, data, **kwargs) -> SupplierPart:
         """Import a supplier part based on the provided data."""
         spp, _ = SupplierPart.objects.get_or_create(
-            SKU=data["sku"],
+            SKU=data.ManufacturerProductNumber,
             supplier=self.supplier_company,
             **kwargs,
-            defaults={"link": data["link"]},
+            defaults={"link": data.ProductUrl},
         )
 
         SupplierPriceBreak.objects.filter(part=spp).delete()
@@ -195,7 +216,7 @@ class DigikeySupplierPlugin(SupplierMixin, InvenTreePlugin):
                 SupplierPriceBreak(
                     part=spp, quantity=quantity, price=price, price_currency=currency
                 )
-                for quantity, (price, currency) in data["price"].items()
+                for quantity, (price, currency) in self.get_pricing_data(data).items()
             ]
         )
 
